8e1e13f16f6cb179d2432428619b379e0d4761c6
Rick Bassham <brodrick.bassham@gmail.com>
2024-09-18 12:59:10 -0400

    fix: code cleanup

--- python_alpaca_server/api/safetymonitor_api.py
+++ python_alpaca_server/api/safetymonitor_api.py
@@ -1,8 +1,7 @@
 from typing import Annotated, List
 
 import structlog
-from fastapi import APIRouter, Depends, HTTPException, Query
-from pydantic import BaseModel
+from fastapi import APIRouter, Depends, Query
 
 from ..device import Device, UrlDeviceType, device_finder
 from ..devices.safety_monitor import SafetyMonitor
@@ -12,26 +11,19 @@ from ..response import Response, common_endpoint_parameters
 logger: structlog.stdlib.BoundLogger = structlog.get_logger(__name__)
 
 
-class Action(BaseModel):
-    Action: str
-    Parameters: str
-
-
 def create_router(devices: List[Device]):
     router = APIRouter()
 
     async def get_issafe(
         req: Annotated[CommonRequest, Query()],
-        device: Device = Depends(device_finder(devices, UrlDeviceType.SafetyMonitor)),
+        device: SafetyMonitor = Depends(
+            device_finder(devices, UrlDeviceType.SafetyMonitor)
+        ),
     ) -> Response[bool]:
-
-        if isinstance(device, SafetyMonitor):
-            return Response[bool].from_request(
-                req,
-                device.get_issafe(req),
-            )
-        else:
-            raise HTTPException(status_code=404)
+        return Response[bool].from_request(
+            req,
+            device.get_issafe(req),
+        )
 
     router.get(
         "/safetymonitor/{device_number}/issafe",
