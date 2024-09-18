b78ed61a4c76193c1eda8e81d6ae86ee6d19e02c
Rick Bassham <brodrick.bassham@gmail.com>
2024-09-18 11:04:26 -0400

    feat: add FilterWheel base class

---
+++ python_alpaca_server/devices/filter_wheel.py
@@ -0,0 +1,28 @@
+from typing import List
+from ..device import Device, DeviceType
+from ..request import (
+    CommonRequest,
+    PutPositionRequest,
+)
+from abc import abstractmethod
+
+
+class FilterWheel(Device):
+    def __init__(self, unique_id: str):
+        super().__init__(DeviceType.FilterWheel, unique_id)
+
+    @abstractmethod
+    def get_focusoffsets(self, req: CommonRequest) -> List[int]:
+        raise NotImplementedError(req)
+
+    @abstractmethod
+    def get_names(self, req: CommonRequest) -> List[str]:
+        raise NotImplementedError(req)
+
+    @abstractmethod
+    def get_position(self, req: CommonRequest) -> int:
+        raise NotImplementedError(req)
+
+    @abstractmethod
+    def put_position(self, req: PutPositionRequest) -> float:
+        raise NotImplementedError(req)
